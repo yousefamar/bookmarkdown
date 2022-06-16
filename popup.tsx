cc3a544f73a8f1b1a00a6f33767f612692f66ecd
Yousef Amar <yousefamar@gmail.com>
2022-06-16 12:50:16 +0100

    add: basics for downloading markdown file

--- popup.tsx
+++ popup.tsx
@@ -3,6 +3,16 @@ import { useState } from "react"
 function IndexPopup() {
   const [data, setData] = useState("")
 
+  const download = async (filename, content) => {
+    // Download text as file
+    const downloadId = await chrome.downloads.download({
+      url: URL.createObjectURL(new Blob([content], { type: "text/markdown; charset=UTF-8" })),
+      filename,
+    });
+
+    console.log(downloadId);
+  };
+
   return (
     <div
       style={{
@@ -14,6 +24,7 @@ function IndexPopup() {
         Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
       </h1>
       <input onChange={(e) => setData(e.target.value)} value={data} />
+      <button onClick={() => download('bookmarks/text.txt', 'Hi there')}>Download</button>
     </div>
   )
 }
